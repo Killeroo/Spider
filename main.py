7461f54379c2cba4764c21ff3a7bde260d98f51c
Stefan Horne <stefan.horne@gmail.com>
2016-12-08 17:15:59 +0000

    Fixed issue with global variables being treated weirdly

--- main.py
+++ main.py
@@ -5,29 +5,27 @@ from domain import *
 from general import *
 import sys
 
-PROJECT_NAME = 'BU'
-HOMEPAGE = 'http://www1.bournemouth.ac.uk'
-DOMAIN_NAME = get_domain_name(HOMEPAGE)
-QUEUE_FILE = PROJECT_NAME + '/queue.txt'
-CRAWLED_FILE = PROJECT_NAME + '/crawled.txt'
-NUMBER_OF_THREADS = 8
 queue = Queue()
 
 
 def run():
     arguments = sys.argv
-    PROJECT_NAME = arguments[1]
-    HOMEPAGE = arguments[2]
-    NUMBER_OF_THREADS = arguments[3]
+    project_name = arguments[1]
+    homepage = arguments[2]
+    number_of_threads = int(arguments[3])
 
-    Spider(PROJECT_NAME, HOMEPAGE, DOMAIN_NAME)
-    create_workers()
-    crawl()
+    domain_name = get_domain_name(homepage)
+    queue_file = project_name + '/queue.txt'
+    # crawled_file = project_name + '/crawled.txt'
+
+    Spider(project_name, homepage, domain_name)
+    create_workers(number_of_threads)
+    crawl(queue_file)
 
 
 # Create worker threads (will die when main exits)
-def create_workers():
-    for _ in range(NUMBER_OF_THREADS):
+def create_workers(number_of_threads):
+    for _ in range(number_of_threads):
         t = threading.Thread(target=work)
         t.daemon = True
         t.start()
@@ -42,19 +40,19 @@ def work():
 
 
 # Each queued link is a new job
-def create_jobs():
-    for link in file_to_set(QUEUE_FILE):
+def create_jobs(queue_file):
+    for link in file_to_set(queue_file):
         queue.put(link)
     queue.join()
-    crawl()
+    crawl(queue_file)
 
 
 # Check if there are items in the queue, if so crawl them
-def crawl():
-    queued_links = file_to_set(QUEUE_FILE)
+def crawl(queue_file):
+    queued_links = file_to_set(queue_file)
     if len(queued_links) > 0:
         print(str(len(queued_links)) + ' links in the queue')
-        create_jobs()
+        create_jobs(queue_file)
 
 
 run()
